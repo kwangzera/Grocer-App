55b4cdfc727e10fe43920c24a4b54fd737abfe63
GeeTransit <geetransit@gmail.com>
2020-03-29 06:43:51 -0400

    Drop and create

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__.split()[0])
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+db.drop_all()
 db.create_all()  # idempotent
 
 from . import convert, models, queries, routes
